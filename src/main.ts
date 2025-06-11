135a50155f71f5ac4ce8762a8fe1975b64bfe171
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-11 11:59:57 +0200

    add: unknown type + type check

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 // start code
+
+let apiRes: unknown = [1, 2, 3];
+
+switch (typeof apiRes) {
+  case 'string':
+    console.log(apiRes.toUpperCase());
+    break;
+  case 'number':
+    console.log(apiRes * 2);
+    break;
+  case 'boolean':
+    apiRes ? console.log('si') : console.log('no');
+    break;
+
+  default:
+    console.log('tipo non supportato');
+    break;
+}
